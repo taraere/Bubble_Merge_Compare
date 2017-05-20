b1c6f7027478b213e054b0b5c73f4f8f136794d0
Tara Elsen <Tara@Tara-Elsens-MacBook.local>
2017-05-20 16:59:14 +0200

    merge sort skeleton

---
+++ mergesort.c
@@ -0,0 +1,34 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+// l = left index, r = right index
+void merge()
+{
+  // create temporary array
+
+  /* copy data to temporary arrays L[] and R[] 
+(first/left list and second/right list) */
+
+  // compare subarrays & merge temporary arrays into array l..r as arr[l..r]
+
+  // add remaining elements of L[] and then of R[]
+}
+
+void mergeSort()
+{
+  // m = (l+r)/2 but to avoid overflow, m = (l+(r-1))/2
+
+  // sort left, then right half
+}
+
+// to test the functions
+int main()
+{
+  //array = {1, 3, 5, 2, 5, 6}
+
+  printf("Inputted array is:\n");
+
+  printf("Once sorted:\n");
+
+  return 0;
+}
